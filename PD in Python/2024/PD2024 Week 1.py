28ead155029a48cf938a989e48ad57808456c421
LorenzNacilla <lnacilla31@gmail.com>
2025-03-13 14:19:05 +0000

    PD2024 Wk 1 in Python upload

---
+++ PD in Python/2024/PD2024 Week 1.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+df = pd.read_csv("G:/My Drive\Personal/Preppin Data/2024/PD 2024 Wk 1 Input.csv")
+
+# Splitting the Flight Details field based on the //
+df2 = df["Flight Details"].str.split("//", expand=True)
+
+# Renaming all of the split fields
+df["Date"] = df2[0]
+df["Flight Number"] = df2[1]
+df["From and to"] = df2[2]
+df["Class"] = df2[3]
+df["Price"] = df2[4]
+
+# Dropping the Flight Details field
+df.drop(columns=["Flight Details"], inplace=True)
+
+# Splitting the From and to based on the -
+df3 = df["From and to"].str.split("-", expand=True)
+
+# Renaming the From and To fields
+df["From"] = df3[0]
+df["To"] = df3[1]
+
+# Dropping the original From and to field
+df.drop(columns=["From and to"], inplace=True)
+
+# Rearranging the columns
+df = df.reindex(columns=["Date", "Flight Number", "From", "To", "Class", "Price", "Flow Card?", "Bags Checked", "Meal Type"])
+
+#Replacing the FLow Card? column with 1 as yes and 0 as no
+df["Flow Card?"] = df["Flow Card?"].apply(lambda x: "Yes" if x== 1 else "No")
+
+# Rows where they have a flow card
+flow_cards = df[df["Flow Card?"] == "Yes"]
+
+# Rows where they don't have a flow card
+non_flow_cards = df[df["Flow Card?"] == "No"]
+
+
+print(flow_cards)
+print(non_flow_cards)
+
+
+
+
+
